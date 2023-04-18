e08f20113c11e5a1323d68bad539104370557072
harinijai99 <harinij@bu.edu>
2023-04-17 22:37:13 -0400

    Draft 3

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,17 @@
 /* WRITE YOUR JS HERE... YOU MAY REQUIRE MORE THAN ONE JS FILE. IF SO SAVE IT SEPARATELY IN THE SCRIPTS DIRECTORY */
+
+const start = document.getElementById("start");
+
+start.addEventListener('click', showMessage);
+
+function showMessage() {
+  window.alert(["Hope this helped! Always remember it's a bad day not a bad life!"]);
+}
+
+const end = document.getElementById("end");
+
+end.addEventListener('click', showText);
+
+function showText() {
+  window.alert(["If these tactics were'nt sufficient we have more resources for you. You got this!"]);
+}
